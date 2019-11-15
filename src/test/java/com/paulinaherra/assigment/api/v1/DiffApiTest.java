5ef0798a3a455ade05c1f6180e3f3a192b108b00
Paulina Herra <paulina.herra@proxibid.com>
2019-11-14 23:14:58 -0600

    Fixing tests -> TDD

--- src/test/java/com/paulinaherra/assigment/api/v1/DiffApiTest.java
+++ src/test/java/com/paulinaherra/assigment/api/v1/DiffApiTest.java
@@ -10,6 +10,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -35,21 +36,21 @@ public class DiffApiTest {
     client
       .post()
       .uri(URI + "/1/right")
+      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
       .body(BodyInserters.fromValue(new DiffRequest().setData("ewogICAgZGF0YTogInRoaXMgaXMgYSBqc29uIgp9")))
       .exchange()
-      .expectStatus().isOk()
-      .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON);
+      .expectStatus().isOk();
   }
 
   @Test
   public void postLeft() throws Exception {
     client
       .post()
-      .uri(URI + "/1/right")
+      .uri(URI + "/1/left")
+      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
       .body(BodyInserters.fromValue(new DiffRequest().setData("ewogICAgZGF0YTogInRoaXMgaXMgYSBqc29uIgp9")))
       .exchange()
-      .expectStatus().isOk()
-      .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON);
+      .expectStatus().isOk();
   }
 
   @Test
@@ -57,6 +58,7 @@ public class DiffApiTest {
     client
       .get()
       .uri(URI + "/1")
+      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
       .exchange()
       .expectStatus().isOk()
       .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON);
